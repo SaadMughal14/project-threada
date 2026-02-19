4032cf00a67b5708d395f2a921d93c5f18bb8005
Saad Mughal <msaadmughal56@gmail.com>
2026-02-19 14:31:13 +0500

    fix: App.tsx import error

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Lenis from 'lenis';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
